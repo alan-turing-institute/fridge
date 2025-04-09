ec7d84944846e6069d40999bcb790a33b43c2012
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-04-09 12:00:30 +0100

    Add minio operator and tenant

--- infra/azure/__main__.py
+++ infra/azure/__main__.py
@@ -7,12 +7,14 @@ from pulumi import ResourceOptions
 from pulumi_azure_native import (
     containerservice, managedidentity, resources
 )
+from pulumi import Output
 import pulumi_tls as tls
 import pulumi_kubernetes as kubernetes
 from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
-from pulumi_kubernetes.core.v1 import Namespace, PersistentVolumeClaim, PersistentVolumeClaimSpecArgs
+from pulumi_kubernetes.core.v1 import Namespace, PersistentVolumeClaim, PersistentVolumeClaimSpecArgs, Secret
 from pulumi_kubernetes.yaml import ConfigFile
 from pulumi_kubernetes.helm.v4 import Chart, RepositoryOptsArgs
+from pulumi_kubernetes.networking.v1 import Ingress
 from pulumi_kubernetes.storage.v1 import StorageClass
 
 
@@ -236,5 +238,161 @@ cert_manager_issuers = ConfigFile(
     opts=ResourceOptions(
         provider=k8s_provider,
         depends_on=[cert_manager, cert_manager_ns, managed_cluster],
+    ),
+)
+
+## Minio
+
+minio_operator_ns = Namespace(
+    "minio-operator-ns",
+    metadata=ObjectMetaArgs(
+        name="minio-operator",
+    ),
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[managed_cluster],
+    ),
+)
+
+minio_operator = Chart(
+    "minio-operator",
+    namespace=minio_operator_ns.metadata.name,
+    chart="operator",
+    repository_opts=RepositoryOptsArgs(
+        repo="https://operator.min.io",
+    ),
+    version="7.0.1",
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[minio_operator_ns, managed_cluster],
+    ),
+)
+
+minio_tenant_ns = Namespace(
+    "minio-tenant-ns",
+    metadata=ObjectMetaArgs(
+        name="argo-artifacts",
+    ),
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[managed_cluster],
+    ),
+)
+
+minio_config_env = Output.all(config.require("minio_root_user"), config.require_secret("minio_root_password")).apply(
+    lambda args:
+        f"export MINIO_BROWSER_REDIRECT_URL=https://{config.require("minio_browser_url")}\n "
+        f"export MINIO_SERVER_URL=http://minio.argo-artifacts.svc.cluster.local\n "
+        f"export MINIO_ROOT_USER={args[0]}\n "
+        f"export MINIO_ROOT_PASSWORD={args[1]}"
+)
+
+minio_env_secret = Secret(
+    "minio-env-secret",
+    metadata=ObjectMetaArgs(
+        name="argo-artifacts-env-configuration",
+        namespace=minio_tenant_ns.metadata.name,
+    ),
+    type="Opaque",
+    string_data={
+        "config.env": minio_config_env,
+    },
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[minio_tenant_ns],
+    ),
+)
+
+minio_tenant = Chart(
+    "minio-tenant",
+    namespace=minio_tenant_ns.metadata.name,
+    chart="tenant",
+    name="argo-artifacts",
+    version="7.0.1",
+    repository_opts=RepositoryOptsArgs(
+        repo="https://operator.min.io",
+    ),
+    values={
+        "tenant": {
+            "name": "argo-artifacts",
+            "buckets": [
+                {"name" : "argo-artifacts"},
+            ],
+            "certificate": {
+                "requestAutoCert" : "true",
+            },
+            "configuration": {
+                "name": "argo-artifacts-env-configuration",
+            },
+            "configSecret": {
+                "name": "argo-artifacts-env-configuration",
+                "accessKey": None,
+                "secretKey": None,
+                "existingSecret" : "true",
+            },
+            "features": {
+                "domains": {
+                    "console": config.require("minio_browser_url"),
+                    "minio": [
+                        "minio.fridge.develop.turingsafehaven.ac.uk/api",
+                        "minio.argo-artifacts.svc.cluster.local"
+                        ],
+                    }
+            },
+            "pools": [
+                {
+                    "servers": 2,
+                    "name": "argo-artifacts-pool-0",
+                    "size": "20Gi",
+                    "volumesPerServer": 2,
+                    "storageClassName": longhorn_storage_class.metadata.name,
+                },
+            ],
+        },
+    },
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[minio_env_secret, minio_operator, minio_tenant_ns, managed_cluster],
+    ),
+)
+
+minio_ingress = Ingress(
+    "minio-ingress",
+    metadata=ObjectMetaArgs(
+        name="minio-ingress",
+        namespace=minio_tenant_ns.metadata.name,
+        annotations={
+            "nginx.ingress.kubernetes.io/proxy-body-size": "0",
+            "nginx.ingress.kubernetes.io/backend-protocol": "HTTPS",
+            "nginx.ingress.kubernetes.io/proxy-ssl-verify": "off",
+        },
+    ),
+    spec={
+        "ingress_class_name": "nginx",
+        "rules": [
+            {
+                "host": "minio.fridge.develop.turingsafehaven.ac.uk",
+                "http": {
+                    "paths": [
+                        {
+                            "path": "/",
+                            "path_type": "Prefix",
+                            "backend": {
+                                "service": {
+                                    "name": "minio",
+                                    "port": {
+                                        "number": 9443,
+                                    },
+                                }
+                            },
+                        }
+                    ]
+                },
+            }
+        ],
+    },
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[minio_tenant],
     ),
 )
