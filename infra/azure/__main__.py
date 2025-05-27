30f76dc2d1f6ab022b932c0e49a01736bf6c69a8
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-05-27 16:37:34 +0100

    add ingress nginx policy to main deployment

--- infra/azure/__main__.py
+++ infra/azure/__main__.py
@@ -923,3 +923,12 @@ network_policy_containerd_config = ConfigFile(
         depends_on=[managed_cluster, configure_containerd_daemonset],
     ),
 )
+
+network_policy_ingress_nginx = ConfigFile(
+    "network_policy_ingress_nginx",
+    file="./k8s/cilium/ingress-nginx.yaml",
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[managed_cluster, ingress_nginx],
+    ),
+)
