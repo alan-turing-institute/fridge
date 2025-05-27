583019516f4a0658977d3a408a4b19e1e8f80c50
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-05-27 14:50:58 +0100

    Add policy for minio tenant

--- infra/azure/__main__.py
+++ infra/azure/__main__.py
@@ -923,3 +923,12 @@ network_policy_containerd_config = ConfigFile(
         depends_on=[managed_cluster, configure_containerd_daemonset],
     ),
 )
+
+network_policy_minio_tenant = ConfigFile(
+    "network_policy_minio_tenant",
+    file="./k8s/cilium/minio-tenant.yaml",
+    opts=ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[managed_cluster, minio_tenant],
+    ),
+)
