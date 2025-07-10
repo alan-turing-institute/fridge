52cb30e569f2cbca111675c52ed616ae0a49d74a
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-07-10 17:41:35 +0100

    Add workflow template class and endpoint to submit workflows from templates

--- fridge-job-api/main.py
+++ fridge-job-api/main.py
@@ -1,10 +1,31 @@
+import json
 import os
 import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from pydantic import BaseModel
 from typing import Annotated
 
+
+class WorkflowTemplate(BaseModel):
+    namespace: str
+    template_name: str
+    parameters: list[dict] | None = None
+
+
 app = FastAPI()
+
+
+def parse_parameters(parameters: list[dict]) -> list[str]:
+    """
+    Parse the parameters from the workflow template into a list of strings.
+    """
+    return [
+        f"{param['name']}={param['value']}"
+        for param in parameters
+        if "name" in param and "value" in param
+    ]
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -61,4 +82,39 @@ async def get_workflow_template(namespace: str, template_name: str):
         headers={"Authorization": f"Bearer {ARGO_TOKEN}"},
     )
     json_data = r.json()
-    return json_data
+    workflow_template = WorkflowTemplate(
+        namespace=namespace,
+        template_name=template_name,
+        parameters=json_data["spec"]["arguments"]["parameters"],
+    )
+    return [json_data, workflow_template]
+
+
+@app.post("/workflowevents/from_template/")
+async def submit_workflow_from_template(workflow_template: WorkflowTemplate):
+    print(
+        parse_parameters(workflow_template.parameters)
+        if workflow_template.parameters
+        else []
+    )
+    r = requests.post(
+        f"https://argo.dawn.fridge.develop.turingsafehaven.ac.uk/api/v1/workflows/{workflow_template.namespace}/submit",
+        verify=False,
+        headers={"Authorization": f"Bearer {ARGO_TOKEN}"},
+        data=json.dumps(
+            {
+                "resourceKind": "WorkflowTemplate",
+                "resourceName": workflow_template.template_name,
+                "submitOptions": {
+                    "parameters": parse_parameters(workflow_template.parameters)
+                    if workflow_template.parameters
+                    else []
+                },
+            }
+        ),
+    )
+    return {
+        "workflow_submitted": workflow_template,
+        "status": r.status_code,
+        "response": r.json(),
+    }
