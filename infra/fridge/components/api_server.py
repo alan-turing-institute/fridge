a61c32b381912b21b4bef92d8ba317c1ac1f3c16
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-07-23 16:51:37 +0100

    Add new component to deploy api server

---
+++ infra/fridge/components/api_server.py
@@ -0,0 +1,48 @@
+from pulumi import ComponentResource, ResourceOptions
+from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
+from pulumi_kubernetes.core.v1 import Namespace, ServiceAccount
+from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
+
+
+class ApiServer(ComponentResource):
+    def __init__(self, name: str, argo_workflows_ns: str, opts=ResourceOptions) -> None:
+        super().__init__("fridge:k8s:ApiServer", name, {}, opts)
+        child_opts = ResourceOptions.merge(opts, ResourceOptions(parent=self))
+
+        api_ns = Namespace(
+            "fridge-api-namespace",
+            metadata=ObjectMetaArgs(name="fridge-api"),
+            opts=child_opts,
+        )
+
+        api_sa = ServiceAccount(
+            "fridge-api-serviceaccount",
+            metadata=ObjectMetaArgs(name="fridge-api-sa"),
+            opts=child_opts,
+        )
+
+        api_server = Deployment(
+            "fridge-api-server",
+            metadata=ObjectMetaArgs(
+                name="fridge-api-server",
+                namespace=api_ns.metadata.name,
+            ),
+            spec=DeploymentSpecArgs(
+                replicas=1,
+                selector={"match_labels": {"app": "fridge-api-server"}},
+                template={
+                    "metadata": {"labels": {"app": "fridge-api-server"}},
+                    "spec": {
+                        "containers": [
+                            {
+                                "name": "fridge-api-server",
+                                "image": "harbor.aks.fridge.develop.turingsafehaven.ac.uk/internal/fridge-api:latest",
+                                "ports": [{"container_port": 8000}],
+                                "service_account_name": "fridge-api-sa",
+                            }
+                        ]
+                    },
+                },
+            ),
+            opts=child_opts,
+        )
