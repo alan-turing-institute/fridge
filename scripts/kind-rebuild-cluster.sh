25088b2406f0ac983abe64d564a7bd3b448aad05
Jake Watson <jake.watson@bristol.ac.uk>
2025-07-28 16:26:24 +0100

    Add helper scripts and just command runner

---
+++ scripts/kind-rebuild-cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# check is cluster exists
+if kind get clusters | grep -q fridge; then
+  echo "Cluster 'fridge' already exists. Rebuilding..."
+  kind delete cluster --name fridge
+else
+  echo "Creating new cluster 'fridge'..."
+fi
+kind create cluster --name fridge --config /workspace/.devcontainer/kind-config.yaml
+kind get kubeconfig --name fridge > /home/vscode/.kube/config
+# fix kubeconfig to use the correct server address
+export KUBE_API_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' fridge-control-plane)
+export KUBE_API_ADDRESS="https://$KUBE_API_IP:6443"
+yq -i '.clusters[0].cluster.server = strenv(KUBE_API_ADDRESS)' /home/vscode/.kube/config
+# allow the devcontainer to access the cluster network
+#check if the fridge-devcontainer is connected to the kind network
+if [ $(docker inspect -f '{{json .NetworkSettings.Networks}}' fridge-devcontainer | jq '. | has("kind")') == "false" ]; then
+  echo "Connecting fridge-devcontainer to kind network..."
+  docker network connect kind fridge-devcontainer
+else
+  echo "fridge-devcontainer is already connected to kind network."
+fi
