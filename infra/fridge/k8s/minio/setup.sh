a6fd45f85b89399482b12ccde159a84f763f2684
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-08-05 11:08:54 +0100

    Delete unneeded files

--- infra/fridge/k8s/minio/setup.sh
+++
@@ -1,6 +0,0 @@
-echo "Creating default S3 policies"
-for policy in /tmp/scripts/*.json; do
-    echo "Creating policy $$policy"
-    mc --insecure admin policy create $minio_alias $$(basename $$policy .json) $$policy
-    echo "Policy $$(basename $$policy .json) created"
-done
