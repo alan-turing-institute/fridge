8dae7558bea9815ed230082e34fc247e0382c0c8
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-08-07 11:31:32 +0100

    Transfer Minio deployment to ObjectStorage component

--- infra/fridge/components/__init__.py
+++ infra/fridge/components/__init__.py
@@ -1,3 +1,4 @@
 from .api_rbac import ApiRbac
 from .network_policies import NetworkPolicies
+from .object_storage import ObjectStorage, ObjectStorageArgs
 from .storage_classes import StorageClasses, StorageClassesArgs
