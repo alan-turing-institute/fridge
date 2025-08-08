22433967aefd554434264deb848176c95dba4560
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-08-08 10:57:29 +0100

    Add workflow server component import

--- infra/fridge/components/__init__.py
+++ infra/fridge/components/__init__.py
@@ -2,3 +2,4 @@ from .api_rbac import ApiRbac
 from .network_policies import NetworkPolicies
 from .object_storage import ObjectStorage, ObjectStorageArgs
 from .storage_classes import StorageClasses, StorageClassesArgs
+from .workflow_server import WorkflowServer, WorkflowServerArgs
