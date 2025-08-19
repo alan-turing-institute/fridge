ba5c6c6da2292f78a6badf02c9c9eb0a2ae4dbde
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-08-19 10:00:26 +0100

    Use match case instead of if else

--- infra/fridge/components/storage_classes.py
+++ infra/fridge/components/storage_classes.py
@@ -33,143 +33,84 @@ class StorageClasses(ComponentResource):
 
         k8s_environment = args.k8s_environment
 
-        if k8s_environment == K8sEnvironment.AKS:
-            storage_class = StorageClass(
-                "fridge_storage_class",
-                allow_volume_expansion=True,
-                metadata=ObjectMetaArgs(
-                    name=STORAGE_CLASS_NAME,
-                ),
-                parameters={
-                    "diskEncryptionSetID": f"/subscriptions/{args.azure_subscription_id}/resourceGroups/{args.azure_resource_group}/providers/Microsoft.Compute/diskEncryptionSets/{args.azure_disk_encryption_set}",
-                    "kind": "managed",
-                    "skuname": "StandardSSD_LRS",
-                },
-                provisioner="disk.csi.azure.com",
-                opts=child_opts,
-            )
-
-            rwm_class_name = "azurefile"
-        elif k8s_environment == K8sEnvironment.DAWN:
-            longhorn_ns = Namespace(
-                "longhorn-system",
-                metadata=ObjectMetaArgs(
-                    name="longhorn-system",
-                    labels={} | PodSecurityStandard.PRIVILEGED.value,
-                ),
-                opts=child_opts,
-            )
-
-            longhorn = Release(
-                "longhorn",
-                namespace=longhorn_ns.metadata.name,
-                chart="longhorn",
-                version="1.9.0",
-                repository_opts=RepositoryOptsArgs(
-                    repo="https://charts.longhorn.io",
-                ),
-                # Add a toleration for the GPU node, to allow Longhorn to schedule pods/create volumes there
-                values={
-                    "global": {
-                        "tolerations": [
-                            {
-                                "key": "gpu.intel.com/i915",
-                                "operator": "Exists",
-                                "effect": "NoSchedule",
-                            }
-                        ]
+        match k8s_environment:
+            case K8sEnvironment.AKS:
+                storage_class = StorageClass(
+                    "fridge_storage_class",
+                    allow_volume_expansion=True,
+                    metadata=ObjectMetaArgs(
+                        name=STORAGE_CLASS_NAME,
+                    ),
+                    parameters={
+                        "diskEncryptionSetID": f"/subscriptions/{args.azure_subscription_id}/resourceGroups/{args.azure_resource_group}/providers/Microsoft.Compute/diskEncryptionSets/{args.azure_disk_encryption_set}",
+                        "kind": "managed",
+                        "skuname": "StandardSSD_LRS",
                     },
-                    "defaultSettings": {
-                        "taintToleration": "gpu.intel.com/i915:NoSchedule"
+                    provisioner="disk.csi.azure.com",
+                    opts=child_opts,
+                )
+
+                rwm_class_name = "azurefile"
+            case K8sEnvironment.DAWN | K8sEnvironment.K3S:
+                longhorn_ns = Namespace(
+                    "longhorn-system",
+                    metadata=ObjectMetaArgs(
+                        name="longhorn-system",
+                        labels={} | PodSecurityStandard.PRIVILEGED.value,
+                    ),
+                    opts=child_opts,
+                )
+
+                longhorn = Release(
+                    "longhorn",
+                    namespace=longhorn_ns.metadata.name,
+                    chart="longhorn",
+                    version="1.9.0",
+                    repository_opts=RepositoryOptsArgs(
+                        repo="https://charts.longhorn.io",
+                    ),
+                    # Add a toleration for the GPU node, to allow Longhorn to schedule pods/create volumes there
+                    values={
+                        "global": {
+                            "tolerations": [
+                                {
+                                    "key": "gpu.intel.com/i915",
+                                    "operator": "Exists",
+                                    "effect": "NoSchedule",
+                                }
+                            ]
+                        },
+                        "defaultSettings": {
+                            "taintToleration": "gpu.intel.com/i915:NoSchedule"
+                        },
+                        "persistence": {"defaultClassReplicaCount": 2},
                     },
-                    "persistence": {"defaultClassReplicaCount": 2},
-                },
-                opts=ResourceOptions.merge(
-                    child_opts,
-                    ResourceOptions(depends_on=[longhorn_ns]),
-                ),
-            )
+                    opts=ResourceOptions.merge(
+                        child_opts,
+                        ResourceOptions(depends_on=[longhorn_ns]),
+                    ),
+                )
 
-            storage_class = StorageClass(
-                "fridge_storage_class",
-                allow_volume_expansion=True,
-                metadata=ObjectMetaArgs(
-                    name=STORAGE_CLASS_NAME,
-                ),
-                parameters={
-                    "dataLocality": "best-effort",
-                    "fsType": "ext4",
-                    "numberOfReplicas": "2",
-                    "staleReplicaTimeout": "2880",
-                },
-                provisioner="driver.longhorn.io",
-                opts=ResourceOptions.merge(
-                    child_opts,
-                    ResourceOptions(depends_on=[longhorn]),
-                ),
-            )
-
-            rwm_class_name = storage_class.metadata.name
-        elif k8s_environment == K8sEnvironment.K3S:
-            longhorn_ns = Namespace(
-                "longhorn-system",
-                metadata=ObjectMetaArgs(
-                    name="longhorn-system",
-                    labels={} | PodSecurityStandard.PRIVILEGED.value,
-                ),
-                opts=child_opts,
-            )
-
-            longhorn = Release(
-                "longhorn",
-                namespace=longhorn_ns.metadata.name,
-                chart="longhorn",
-                version="1.9.0",
-                repository_opts=RepositoryOptsArgs(
-                    repo="https://charts.longhorn.io",
-                ),
-                # Add a toleration for the GPU node, to allow Longhorn to schedule pods/create volumes there
-                values={
-                    "global": {
-                        "tolerations": [
-                            {
-                                "key": "gpu.intel.com/i915",
-                                "operator": "Exists",
-                                "effect": "NoSchedule",
-                            }
-                        ]
+                storage_class = StorageClass(
+                    "fridge_storage_class",
+                    allow_volume_expansion=True,
+                    metadata=ObjectMetaArgs(
+                        name=STORAGE_CLASS_NAME,
+                    ),
+                    parameters={
+                        "dataLocality": "best-effort",
+                        "fsType": "ext4",
+                        "numberOfReplicas": "2",
+                        "staleReplicaTimeout": "2880",
                     },
-                    "defaultSettings": {
-                        "taintToleration": "gpu.intel.com/i915:NoSchedule"
-                    },
-                    "persistence": {"defaultClassReplicaCount": 2},
-                },
-                opts=ResourceOptions.merge(
-                    child_opts,
-                    ResourceOptions(depends_on=[longhorn_ns]),
-                ),
-            )
+                    provisioner="driver.longhorn.io",
+                    opts=ResourceOptions.merge(
+                        child_opts,
+                        ResourceOptions(depends_on=[longhorn]),
+                    ),
+                )
 
-            storage_class = StorageClass(
-                "fridge_storage_class",
-                allow_volume_expansion=True,
-                metadata=ObjectMetaArgs(
-                    name=STORAGE_CLASS_NAME,
-                ),
-                parameters={
-                    "dataLocality": "best-effort",
-                    "fsType": "ext4",
-                    "numberOfReplicas": "2",
-                    "staleReplicaTimeout": "2880",
-                },
-                provisioner="driver.longhorn.io",
-                opts=ResourceOptions.merge(
-                    child_opts,
-                    ResourceOptions(depends_on=[longhorn]),
-                ),
-            )
-
-            rwm_class_name = storage_class.metadata.name
+                rwm_class_name = storage_class.metadata.name
 
         self.encrypted_storage_class = storage_class
         self.rwm_class_name = rwm_class_name
