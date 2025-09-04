f2a0b3100efa13693fe3d6650cba4b8e7e05698c
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-09-04 16:01:09 +0100

    Add intial configuration for K3s, Dawn, and AKS

--- infra/fridge/components/ingress.py
+++ infra/fridge/components/ingress.py
@@ -1,13 +1,14 @@
 from pulumi import ComponentResource, ResourceOptions
 from pulumi_kubernetes.core.v1 import Namespace
+from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs
+from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 
-from enums import K8sEnvironment
+from enums import K8sEnvironment, PodSecurityStandard
 
 
 class IngressArgs:
-    def __init__(self, k8s_environment: K8sEnvironment, ingress_ns: Namespace):
+    def __init__(self, k8s_environment: K8sEnvironment):
         self.k8s_environment = k8s_environment
-        self.ingress_ns = ingress_ns
 
 
 class Ingress(ComponentResource):
@@ -17,12 +18,43 @@ class Ingress(ComponentResource):
         k8s_environment = args.k8s_environment
 
         match k8s_environment:
-            case K8sEnvironment.AKS:
-                # AKS specific configuration
-                pass
+            case K8sEnvironment.AKS | K8sEnvironment.K3S:
+                ingress_nginx_ns = Namespace(
+                    "ingress-nginx-ns",
+                    metadata=ObjectMetaArgs(
+                        name="ingress-nginx",
+                        labels={} | PodSecurityStandard.RESTRICTED.value,
+                    ),
+                )
             case K8sEnvironment.DAWN:
                 # Dawn specific configuration
-                pass
+                ingress_nginx_ns = Namespace.get("ingress-nginx-ns", "ingress-nginx")
+                ingress_nginx = Release.get("ingress-nginx", "ingress-nginx")
+
+        match k8s_environment:
             case K8sEnvironment.K3S:
-                # K3S specific configuration
-                pass
+                ingress_nginx = Release(
+                    "ingress-nginx",
+                    ReleaseArgs(
+                        chart="ingress-nginx",
+                        version="4.13.2",
+                        repository_opts={
+                            "repo": "https://kubernetes.github.io/ingress-nginx"
+                        },
+                        namespace=ingress_nginx_ns.metadata.name,
+                        create_namespace=False,
+                        values={
+                            "controller": {
+                                "metrics": {
+                                    "enabled": True,
+                                    "serviceMonitor": {"enabled": True},
+                                }
+                            },
+                        },
+                    ),
+                    opts=ResourceOptions(parent=self),
+                )
+
+        self.register_outputs(
+            {"ingress_nginx_ns": ingress_nginx_ns, "ingress_nginx": ingress_nginx}
+        )
