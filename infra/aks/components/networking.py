701c2d54f80597ef5a540d11e59a5520c64aa6fc
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-09-25 10:48:19 +0100

    Add peering and NSGs

--- infra/aks/components/networking.py
+++ infra/aks/components/networking.py
@@ -17,16 +17,77 @@ class Networking(ComponentResource):
         super().__init__("fridge_aks:Networking", name, {}, opts)
         child_opts = ResourceOptions.merge(opts, ResourceOptions(parent=self))
 
-        access_vnet_cidr = args.config.require("access_vnet_cidr")
-        access_nodes_cidr = args.config.require("access_nodes_subnet_cidr")
+        # Create NSGs
 
-        private_vnet_cidr = args.config.require("private_vnet_cidr")
-        private_nodes_cidr = args.config.require("private_nodes_subnet_cidr")
+        private_nsg = network.NetworkSecurityGroup(
+            f"{name}-private-nsg",
+            resource_group_name=args.resource_group_name,
+            location=args.location,
+            network_security_group_name=f"{name}-private-nsg",
+            security_rules=[
+                network.SecurityRuleArgs(
+                    name="AllowK8sAPIInBound",
+                    priority=100,
+                    direction=network.SecurityRuleDirection.INBOUND,
+                    access=network.SecurityRuleAccess.ALLOW,
+                    protocol=network.SecurityRuleProtocol.ASTERISK,
+                    source_port_range="*",
+                    destination_port_range="6443",
+                    source_address_prefix="10.10.0.0/16",  # Access cluster VNet
+                    destination_address_prefix="*",
+                )
+            ],
+            opts=child_opts,
+        )
 
+        access_nsg = network.NetworkSecurityGroup(
+            f"{name}-access-nsg",
+            resource_group_name=args.resource_group_name,
+            location=args.location,
+            network_security_group_name=f"{name}-access-nsg",
+            security_rules=[
+                network.SecurityRuleArgs(
+                    name="AllowK8sAPIInBound",
+                    priority=100,
+                    direction=network.SecurityRuleDirection.INBOUND,
+                    access=network.SecurityRuleAccess.ALLOW,
+                    protocol=network.SecurityRuleProtocol.ASTERISK,
+                    source_port_range="*",
+                    destination_port_range="6443",
+                    source_address_prefix="*",
+                    destination_address_prefix="*",
+                ),
+                network.SecurityRuleArgs(
+                    name="AllowHTTPInBound",
+                    priority=200,
+                    direction=network.SecurityRuleDirection.INBOUND,
+                    access=network.SecurityRuleAccess.ALLOW,
+                    protocol=network.SecurityRuleProtocol.ASTERISK,
+                    source_port_range="*",
+                    destination_port_range="80",
+                    source_address_prefix="*",
+                    destination_address_prefix="*",
+                ),
+                network.SecurityRuleArgs(
+                    name="AllowHTTPSInBound",
+                    priority=300,
+                    direction=network.SecurityRuleDirection.INBOUND,
+                    access=network.SecurityRuleAccess.ALLOW,
+                    protocol=network.SecurityRuleProtocol.ASTERISK,
+                    source_port_range="*",
+                    destination_port_range="443",
+                    source_address_prefix="*",
+                    destination_address_prefix="*",
+                ),
+            ],
+            opts=child_opts,
+        )
+
+        # Create VNets and node subnets for each cluster
         access_vnet = network.VirtualNetwork(
             f"{name}-access-vnet",
             resource_group_name=args.resource_group_name,
-            address_space=network.AddressSpaceArgs(address_prefixes=[access_vnet_cidr]),
+            address_space=network.AddressSpaceArgs(address_prefixes=["10.10.0.0/16"]),
             opts=child_opts,
         )
 
@@ -34,7 +95,8 @@ class Networking(ComponentResource):
             f"{name}-access-nodes",
             resource_group_name=args.resource_group_name,
             virtual_network_name=access_vnet.name,
-            address_prefix=access_nodes_cidr,
+            address_prefix="10.10.1.0/24",
+            network_security_group=network.NetworkSecurityGroupArgs(id=access_nsg.id),
             opts=ResourceOptions.merge(
                 child_opts, ResourceOptions(depends_on=[access_vnet])
             ),
@@ -43,9 +105,7 @@ class Networking(ComponentResource):
         private_vnet = network.VirtualNetwork(
             f"{name}-private-vnet",
             resource_group_name=args.resource_group_name,
-            address_space=network.AddressSpaceArgs(
-                address_prefixes=[private_vnet_cidr]
-            ),
+            address_space=network.AddressSpaceArgs(address_prefixes=["10.20.0.0/16"]),
             opts=child_opts,
         )
 
@@ -53,18 +113,60 @@ class Networking(ComponentResource):
             f"{name}-private-nodes",
             resource_group_name=args.resource_group_name,
             virtual_network_name=private_vnet.name,
-            address_prefix=private_nodes_cidr,
+            address_prefix="10.20.1.0/24",
+            network_security_group=network.NetworkSecurityGroupArgs(id=private_nsg.id),
             opts=ResourceOptions.merge(
                 child_opts, ResourceOptions(depends_on=[private_vnet])
             ),
         )
 
+        # Set up VNet peering between the two VNets so they can communicate
+        private_to_access_peering = network.VirtualNetworkPeering(
+            f"{name}-private-to-access-peering",
+            resource_group_name=args.resource_group_name,
+            virtual_network_name=private_vnet.name,
+            remote_virtual_network=network.SubResourceArgs(id=access_vnet.id),
+            allow_virtual_network_access=True,
+            allow_forwarded_traffic=True,
+            allow_gateway_transit=False,
+            use_remote_gateways=False,
+            opts=ResourceOptions.merge(
+                child_opts,
+                ResourceOptions(
+                    depends_on=[private_vnet, private_nodes, access_vnet, access_nodes]
+                ),
+            ),
+        )
+
+        access_to_private_peering = network.VirtualNetworkPeering(
+            f"{name}-access-to-private-peering",
+            resource_group_name=args.resource_group_name,
+            virtual_network_name=access_vnet.name,
+            remote_virtual_network=network.SubResourceArgs(id=private_vnet.id),
+            allow_virtual_network_access=True,
+            allow_forwarded_traffic=True,
+            allow_gateway_transit=False,
+            use_remote_gateways=False,
+            opts=ResourceOptions.merge(
+                child_opts,
+                ResourceOptions(
+                    depends_on=[private_vnet, private_nodes, access_vnet, access_nodes]
+                ),
+            ),
+        )
+
+        self.access_vnet_id = access_vnet.id
         self.access_nodes_subnet_id = access_nodes.id
+        self.private_vnet_id = private_vnet.id
         self.private_nodes_subnet_id = private_nodes.id
 
         self.register_outputs(
             {
                 "access_vnet": access_vnet,
                 "private_vnet": private_vnet,
+                "access_nodes_subnet": access_nodes,
+                "private_nodes_subnet": private_nodes,
+                "access_to_private_peering": access_to_private_peering,
+                "private_to_access_peering": private_to_access_peering,
             }
         )
