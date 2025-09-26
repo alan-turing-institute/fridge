6b695e936d07183c7cbeeb6c5849288622f7c8b5
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-09-26 16:07:46 +0100

    Pass minio resources to MinioConfigJob

--- infra/fridge/__main__.py
+++ infra/fridge/__main__.py
@@ -121,8 +121,8 @@ minio = components.ObjectStorage(
 minio_config = components.MinioConfigJob(
     name=f"{stack_name}-minio-config-job",
     args=components.MinioConfigArgs(
-        minio_tenant_ns=minio,
-        minio_tenant=minio,
+        minio_tenant_ns=minio.minio_tenant_ns,
+        minio_tenant=minio.minio_tenant,
         minio_credentials={
             "minio_root_user": config.require_secret("minio_root_user"),
             "minio_root_password": config.require_secret("minio_root_password"),
