2bdcc12d2a5dd265de9c290bf84c93c7ab37e161
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-10-20 13:42:07 +0100

    Remove dual cluster testing import

--- infra/aks_dual_cluster/components/__init__.py
+++ infra/aks_dual_cluster/components/__init__.py
@@ -1,4 +1,3 @@
 from .access_cluster import AccessCluster, AccessClusterArgs
-from .dual_cluster_test import DualCluster, DualClusterArgs
 from .networking import Networking, NetworkingArgs
 from .isolated_cluster import IsolatedCluster, IsolatedClusterArgs
