f2889b95bcbffd64c9d5d662038cfdd6c06d107f
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-11-11 10:47:28 +0000

    Create registry endpoint and docker proxy project

--- infra/fridge-dual-cluster/access-cluster/components/container_registry.py
+++ infra/fridge-dual-cluster/access-cluster/components/container_registry.py
@@ -236,19 +236,31 @@ class ContainerRegistry(ComponentResource):
                                     """
                                     #!/bin/sh
                                     echo "Configuring Harbor..."
-                                    # Example: Add additional configuration commands here
-                                    # curl -k PUT -u "$HARBOR_ADMIN_USER:$HARBOR_ADMIN_PASSWORD" -H "Content-Type: application/json" -ki "$HARBOR_URL/api/v.2configurations" -d '{"isolated-cluster":"newpassword"}'
                                     curl -X 'POST' \
                                         -u "$HARBOR_ADMIN_USER:$HARBOR_ADMIN_PASSWORD" \
-                                        'http://10.0.50.50/api/v2.0/users' \
+                                        'http://10.0.50.50/api/v2.0/registries' \
                                         -H 'accept: application/json' \
                                         -H 'Content-Type: application/json' \
                                         -d '{
-                                            "email": "string",
-                                            "realname": "string",
-                                            "comment": "string",
-                                            "password": "string",
-                                            "username": "string"
+                                            "id": 1,
+                                            "url": "https://hub.docker.com",
+                                            "type": "docker-hub",
+                                            "credential": {},
+                                            "name": "DockerHub",
+                                            }'
+                                    curl -X 'POST' \
+                                        -u "$HARBOR_ADMIN_USER:$HARBOR_ADMIN_PASSWORD" \
+                                        'http://10.0.50.50/api/v2.0/projects' \
+                                        -H 'accept: application/json' \
+                                        -H 'Content-Type: application/json' \
+                                        -d '{
+                                            "project_name": "docker-proxy",
+                                            "public": true,
+                                            "cve_allowlist": {},
+                                            "registry_id": 1,
+                                            "metadata": {
+                                                "proxy_speed_kb": "-1",
+                                                }
                                             }'
                                     """,
                                 ],
