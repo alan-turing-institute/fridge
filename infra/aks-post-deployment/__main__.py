6bf0f5f9afa824a26b500c9ce7f9f14def303338
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-11-27 16:19:17 +0000

    More specific variable names

--- infra/aks-post-deployment/__main__.py
+++ infra/aks-post-deployment/__main__.py
@@ -6,16 +6,16 @@ config = pulumi.Config()
 # Import infrastructure stack outputs
 organization = config.require("organization_name")
 project_name = config.require("project_name")
-stack = config.require("infrastructure_stack_name")
+infrastructure_stack_name = config.require("infrastructure_stack_name")
 infrastructure_stack_reference = pulumi.StackReference(
-    f"{organization}/{project_name}/{stack}"
+    f"{organization}/{project_name}/{infrastructure_stack_name}"
 )
 
 # Import access cluster stack outputs
-access_stack = config.require("access_cluster_stack_name")
+access_stack_name = config.require("access_cluster_stack_name")
 access_project = "fridge-access"
 access_stack_reference = pulumi.StackReference(
-    f"{organization}/{access_project}/{access_stack}"
+    f"{organization}/{access_project}/{access_stack_name}"
 )
 
 access_nodes_subnet_cidr = infrastructure_stack_reference.get_output(
