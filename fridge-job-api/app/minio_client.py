a828a74542e6339e698c86fc56eee7a6e1f21aea
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-12-02 15:36:30 +0000

    Always refresh token for testing purposes

--- fridge-job-api/app/minio_client.py
+++ fridge-job-api/app/minio_client.py
@@ -110,7 +110,8 @@ class MinioClient:
         """Check if the service account token has changed since last read."""
         try:
             current_token = Path(self.SA_TOKEN_FILE).read_text().strip()
-            return current_token != self._last_token
+            return True
+            # return current_token != self._last_token
         except Exception as e:
             print(f"Error reading token file: {e}")
             return False
