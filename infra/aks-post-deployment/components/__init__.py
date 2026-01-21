24f886e2847ab25667784bf99e69f342db114663
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-01-21 15:08:24 +0000

    Add stackouputsArgs to init

--- infra/aks-post-deployment/components/__init__.py
+++ infra/aks-post-deployment/components/__init__.py
@@ -1,3 +1,3 @@
 from .dns_zone import PrivateDNSZone
 from .network_security_rules import NetworkSecurityRules, NetworkSecurityRulesArgs
-from .stack_outputs import StackOutputs
+from .stack_outputs import StackOutputs, StackOutputsArgs
