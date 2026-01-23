51fe2e71fed563dd133086cfd3612f99c7e1c0e9
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-01-23 16:44:39 +0000

    Add k8s API server ip to stack outputs

--- infra/aks-post-deployment/components/stack_outputs.py
+++ infra/aks-post-deployment/components/stack_outputs.py
@@ -45,12 +45,14 @@ class StackOutputs(ComponentResource):
         self.access_subnet_nsg = self.infrastructure_stack.get_output(
             "access_subnet_nsg"
         )
-
-        self.isolated_subnet_nsg = self.infrastructure_stack.get_output(
-            "isolated_subnet_nsg"
+        self.fridge_api_ip = self.isolated_stack.get_output("fridge_api_ip")
+        self.harbor_ip_address = self.access_stack.get_output("harbor_ip_address")
+        self.isolated_cluster_api_server_ip = self.infrastructure_stack.get_output(
+            "isolated_cluster_api_server_ip"
         )
         self.isolated_nodes_subnet_cidr = self.infrastructure_stack.get_output(
             "isolated_nodes_subnet_cidr"
         )
-
-        self.harbor_ip_address = self.access_stack.get_output("harbor_ip_address")
+        self.isolated_subnet_nsg = self.infrastructure_stack.get_output(
+            "isolated_subnet_nsg"
+        )
