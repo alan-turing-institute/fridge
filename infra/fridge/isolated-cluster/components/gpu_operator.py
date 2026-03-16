6093a824c6c1c14286b1a0e841018c08bccbc87b
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-03-16 16:32:08 +0000

    Install node feature discovery and gpu device

--- infra/fridge/isolated-cluster/components/gpu_operator.py
+++ infra/fridge/isolated-cluster/components/gpu_operator.py
@@ -4,6 +4,7 @@ from pulumi_kubernetes.core.v1 import Namespace
 from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs
 from pulumi_kubernetes.helm.v4 import RepositoryOptsArgs
 from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
+from pulumi_kubernetes.yaml import ConfigFile
 
 from enums import K8sEnvironment
 
@@ -25,7 +26,38 @@ class GPUOperator(ComponentResource):
         super().__init__("fridge:k8s:GPUOperator", name, {}, opts)
         child_opts = ResourceOptions.merge(opts, ResourceOptions(parent=self))
 
+        self.node_feature_discovery_ns = Namespace(
+            "node-feature-discovery-ns",
+            metadata=ObjectMetaArgs(
+                name="node-feature-discovery",
+            ),
+            opts=child_opts,
+        )
+
+        self.node_feature_discovery = Release(
+            "node-feature-discovery",
+            ReleaseArgs(
+                name="node-feature-discovery",
+                namespace=self.node_feature_discovery_ns.metadata.name,
+                chart="node-feature-discovery",
+                version="0.18.3",
+                repository_opts=RepositoryOptsArgs(
+                    repo="https://kubernetes-sigs.github.io/node-feature-discovery/charts",
+                ),
+            ),
+            opts=child_opts,
+        )
+
         if args.k8s_environment == K8sEnvironment.DAWN:
+
+            self.intel_node_feature_rules = ConfigFile(
+                "intel-node-feature-rules",
+                file="https://raw.githubusercontent.com/intel/intel-device-plugins-for-kubernetes/main/deployments/nfd/overlays/node-feature-rules/node-feature-rules.yaml",
+                opts=ResourceOptions.merge(
+                    child_opts, ResourceOptions(depends_on=self.node_feature_discovery)
+                ),
+            )
+
             self.gpu_operator_ns = Namespace(
                 "gpu-operator-ns",
                 metadata=ObjectMetaArgs(
@@ -38,12 +70,32 @@ class GPUOperator(ComponentResource):
             self.gpu_operator = Release(
                 "intel-gpu-operator",
                 ReleaseArgs(
+                    name="intel-gpu-operator",
                     namespace=self.gpu_operator_ns.metadata.name,
                     chart="intel-device-plugins-operator",
                     version="0.35.0",
                     repository_opts=RepositoryOptsArgs(
                         repo="https://intel.github.io/helm-charts/",
                     ),
+                    values={
+                        "tolerations": [
+                            {
+                                "key": "gpu.intel.com/i915",
+                                "operator": "Exists",
+                                "effect": "NoSchedule",
+                            }
+                        ]
+                    },
                 ),
-                opts=child_opts,
+                opts=ResourceOptions.merge(
+                    child_opts, ResourceOptions(depends_on=self.node_feature_discovery)
+                ),
+            )
+
+            self.gpu_device = ConfigFile(
+                "gpu-device",
+                file="./k8s/gpu_operator/intel-gpu-device.yaml",
+                opts=ResourceOptions.merge(
+                    child_opts, ResourceOptions(depends_on=self.gpu_operator)
+                ),
             )
