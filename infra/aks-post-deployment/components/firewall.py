a021fd214f0021e51211543b4b5efb1eb6644f1c
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-05-22 16:12:39 +0100

    import stackoutputs for creating firewall

--- infra/aks-post-deployment/components/firewall.py
+++ infra/aks-post-deployment/components/firewall.py
@@ -1,4 +1,5 @@
 import pulumi
+from .stack_outputs import StackOutputs
 from pulumi import ComponentResource, ResourceOptions
 from pulumi_azure_native import network
 
@@ -9,10 +10,12 @@ class FirewallArgs:
         config: pulumi.Config,
         location: str,
         resource_group_name: str,
+        stack_outputs: StackOutputs,
     ) -> None:
         self.config = config
         self.location = location
         self.resource_group_name = resource_group_name
+        self.stack_outputs = stack_outputs
 
 
 class Firewall(ComponentResource):
@@ -21,6 +24,15 @@ class Firewall(ComponentResource):
     ) -> None:
         super().__init__("fridge:aks-post-deployment:firewall", name, {}, opts)
         child_opts = ResourceOptions.merge(opts, ResourceOptions(parent=self))
+
+        # Create subnet for firewall
+        self.firewall_subnet = network.Subnet(
+            "fridge-firewall-subnet",
+            resource_group_name=args.resource_group_name,
+            virtual_network_name=args.stack_outputs.isolated_vnet_name,
+            address_prefix="10.20.2.0/24",
+            opts=child_opts,
+        )
 
         # Create Azure Firewall
         # A firewall is necessary to support node bootstrapping in the isolated cluster
