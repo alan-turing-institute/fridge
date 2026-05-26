24c9302cfc09056d22be23a6e5949c448ddbd6be
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-05-26 21:18:50 +0100

    Add additional enum class for firewall rules

--- infra/aks-post-deployment/enums/__init__.py
+++ infra/aks-post-deployment/enums/__init__.py
@@ -6,3 +6,11 @@ class FridgeStack(Enum):
     INFRASTRUCTURE = "infrastructure"
     ACCESS = "access"
     ISOLATED = "isolated"
+
+
+@unique
+class AksCoreOutboundFqdn(str, Enum):
+    AZURE_MGMT = "management.azure.com"
+    MCR = "mcr.microsoft.com"
+    PACKAGES_AKS_AZURE_COM = "packages.aks.azure.com"
+    MS_LOGIN = "mslogin.microsoft.com"
