c85270e02fa20851e0d9a50fb1962e6f17a73cb4
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-07-22 12:11:04 +0100

    Use stable pvc for netbird, add internal svc

--- infra/fridge/access-cluster/components/vpn_server.py
+++ infra/fridge/access-cluster/components/vpn_server.py
@@ -1,20 +1,30 @@
 import pulumi
 from pulumi import ComponentResource, ResourceOptions
-from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
+from pulumi_kubernetes.apps.v1 import (
+    Deployment,
+    DeploymentSpecArgs,
+    DeploymentStrategyArgs,
+)
 from pulumi_kubernetes.core.v1 import (
     CapabilitiesArgs,
     ConfigMap,
     ConfigMapVolumeSourceArgs,
     ContainerArgs,
     ContainerPortArgs,
-    EmptyDirVolumeSourceArgs,
     EnvVarArgs,
     Namespace,
+    PersistentVolumeClaim,
+    PersistentVolumeClaimSpecArgs,
+    PersistentVolumeClaimVolumeSourceArgs,
     PodSpecArgs,
     PodTemplateSpecArgs,
     SecurityContextArgs,
+    Service,
+    ServicePortArgs,
+    ServiceSpecArgs,
     VolumeArgs,
     VolumeMountArgs,
+    VolumeResourceRequirementsArgs,
 )
 from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
 
@@ -72,12 +82,25 @@ frontend home_tre_in
     default_backend home_tre_out
 
 backend home_tre_out
-    server home_tre 100.99.123.30:8000 check
+    server home_tre test-netbird-deploy.netbird.cloud:8000 check
 """,
             },
         )
 
         netbird_config = args.config.require_object("netbird")
+
+        self.netbird_data_volume = PersistentVolumeClaim(
+            "netbird-data",
+            metadata=ObjectMetaArgs(
+                namespace=self.vpn_ns.metadata.name,
+                name="netbird-data",
+            ),
+            spec=PersistentVolumeClaimSpecArgs(
+                access_modes=["ReadWriteOnce"],
+                resources=VolumeResourceRequirementsArgs(requests={"storage": "100Mi"}),
+            ),
+            opts=child_opts,
+        )
 
         self.vpn_deployment = Deployment(
             "netbird-proxy",
@@ -86,6 +109,7 @@ backend home_tre_out
             ),
             spec=DeploymentSpecArgs(
                 selector=LabelSelectorArgs(match_labels={"app": "netbird-proxy"}),
+                strategy=DeploymentStrategyArgs(type="Recreate"),
                 replicas=1,
                 template=PodTemplateSpecArgs(
                     metadata=ObjectMetaArgs(
@@ -146,7 +170,9 @@ backend home_tre_out
                         volumes=[
                             VolumeArgs(
                                 name="netbird-data",
-                                empty_dir=EmptyDirVolumeSourceArgs(),
+                                persistent_volume_claim=PersistentVolumeClaimVolumeSourceArgs(
+                                    claim_name=self.netbird_data_volume.metadata.name
+                                ),
                             ),
                             VolumeArgs(
                                 name="haproxy-config",
@@ -160,6 +186,38 @@ backend home_tre_out
             ),
             opts=ResourceOptions.merge(
                 child_opts,
-                ResourceOptions(depends_on=[self.haproxy_config, self.vpn_ns]),
+                ResourceOptions(
+                    depends_on=[
+                        self.haproxy_config,
+                        self.netbird_data_volume,
+                        self.vpn_ns,
+                    ]
+                ),
+            ),
+        )
+
+        # this service exposes the netbird proxy internally, but only to the access cluster
+        # ideally, want to point the internal load balancer at this, so the isolated cluster can talk to it
+
+        self.netbird_svc = Service(
+            "netbird-proxy-svc",
+            metadata=ObjectMetaArgs(
+                namespace=self.vpn_ns.metadata.name,
+                name="netbird-proxy-svc",
+            ),
+            spec=ServiceSpecArgs(
+                selector=LabelSelectorArgs(match_labels={"app": "netbird-proxy"}),
+                ports=[
+                    ServicePortArgs(
+                        name="vpn-port",
+                        port=8000,
+                        target_port=8000,
+                        protocol="TCP",
+                    )
+                ],
+                type="ClusterIP",
+            ),
+            opts=ResourceOptions.merge(
+                child_opts, ResourceOptions(depends_on=[self.vpn_deployment])
             ),
         )
