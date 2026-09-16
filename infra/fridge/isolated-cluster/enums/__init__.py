3a763318bb55871bc3424163643b5403fc3d8256
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-09-16 15:02:45 +0100

    Update Argo to 3.7.11

--- infra/fridge/isolated-cluster/enums/__init__.py
+++ infra/fridge/isolated-cluster/enums/__init__.py
@@ -29,7 +29,7 @@ tls_issuer_names = {
 
 
 class SoftwareVersion(Enum):
-    ARGO_WORKFLOWS = "0.45.20"  # Corresponds to Argo Workflows v3.6.10
+    ARGO_WORKFLOWS = "0.47.5"  # Corresponds to Argo Workflows v3.7.11
     CERT_MANAGER = "1.17.1"
     FRIDGE_API = "0.5.1"
     INTEL_GPU_OPERATOR = "0.35.0"
